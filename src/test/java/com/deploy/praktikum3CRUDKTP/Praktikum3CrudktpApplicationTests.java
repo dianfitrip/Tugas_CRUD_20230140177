40a873fb988677184bed020e662b8d4f5eb85e81
dianfitrip <dianfitri683@gmail.com>
2026-03-13 10:36:18 +0700

    Initial commit

---
+++ src/test/java/com/deploy/praktikum3CRUDKTP/Praktikum3CrudktpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deploy.praktikum3CRUDKTP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praktikum3CrudktpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
