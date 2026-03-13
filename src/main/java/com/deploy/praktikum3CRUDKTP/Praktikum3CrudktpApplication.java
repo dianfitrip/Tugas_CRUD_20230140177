6f9746a9245a602e03486c33d170f256cbe0ce72
dianfitrip <dianfitri683@gmail.com>
2026-03-13 10:46:19 +0700

    main app

---
+++ src/main/java/com/deploy/praktikum3CRUDKTP/Praktikum3CrudktpApplication.java
@@ -0,0 +1,13 @@
+package com.deploy.praktikum3CRUDKTP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktikum3CrudktpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Praktikum3CrudktpApplication.class, args);
+	}
+
+}
