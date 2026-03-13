7834241563626c7a6724a91133e3642d9a4112f2
dianfitrip <dianfitri683@gmail.com>
2026-03-13 10:40:29 +0700

    Menambahkan entity Ktp untuk tabel ktp menggunakan JPA

---
+++ src/main/java/com/deploy/praktikum3CRUDKTP/entity/Ktp.java
@@ -0,0 +1,33 @@
+package com.deploy.praktikum3CRUDKTP.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "ktp")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ktp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nomor_ktp", unique = true)
+    private String nomorKtp;
+
+    @Column(name = "nama_lengkap")
+    private String namaLengkap;
+
+    private String alamat;
+
+    @Column(name = "tanggal_lahir")
+    private Date tanggalLahir;
+
+    @Column(name = "jenis_kelamin")
+    private String jenisKelamin;
+}
