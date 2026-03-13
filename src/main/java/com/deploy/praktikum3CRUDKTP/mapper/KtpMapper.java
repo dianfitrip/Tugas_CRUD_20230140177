b071e9799d136ccd40e23fc3e7d8a1a3f807fecb
dianfitrip <dianfitri683@gmail.com>
2026-03-13 10:43:24 +0700

    Menambahkan KtpMapper untuk konversi antara Entity dan DTO

---
+++ src/main/java/com/deploy/praktikum3CRUDKTP/mapper/KtpMapper.java
@@ -0,0 +1,35 @@
+package com.deploy.praktikum3CRUDKTP.mapper;
+
+import com.deploy.praktikum3CRUDKTP.dto.KtpDTO;
+import com.deploy.praktikum3CRUDKTP.entity.Ktp;
+
+public class KtpMapper {
+
+    public static Ktp toEntity(KtpDTO dto) {
+
+        Ktp ktp = new Ktp();
+
+        ktp.setId(dto.getId());
+        ktp.setNomorKtp(dto.getNomorKtp());
+        ktp.setNamaLengkap(dto.getNamaLengkap());
+        ktp.setAlamat(dto.getAlamat());
+        ktp.setTanggalLahir(dto.getTanggalLahir());
+        ktp.setJenisKelamin(dto.getJenisKelamin());
+
+        return ktp;
+    }
+
+    public static KtpDTO toDTO(Ktp entity) {
+
+        KtpDTO dto = new KtpDTO();
+
+        dto.setId(entity.getId());
+        dto.setNomorKtp(entity.getNomorKtp());
+        dto.setNamaLengkap(entity.getNamaLengkap());
+        dto.setAlamat(entity.getAlamat());
+        dto.setTanggalLahir(entity.getTanggalLahir());
+        dto.setJenisKelamin(entity.getJenisKelamin());
+
+        return dto;
+    }
+}
